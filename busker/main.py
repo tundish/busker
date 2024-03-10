e635de1211f95e6debbb467c0bb280cbb76139bd
tundish <tundish@gigeconomy.org.uk>
2024-03-10 17:48:52 +0000

    Quick demo of discover_assets.

--- busker/main.py
+++ busker/main.py
@@ -1,12 +1,28 @@
+import argparse
 import sys
+
+from balladeer import discover_assets
 
 import busker
 
 
-def run():
-    print(f"Busker {busker.__version__}", file=sys.stdout)
+def main(args):
+    assets = discover_assets(args.source, "")
+    print(f"Busker {busker.__version__}", file=sys.stderr)
+    print(f"{assets}", file=sys.stdout)
     print("Done.", file=sys.stderr)
-    sys.exit(0)
+    return 0
+
+def parser():
+    rv = argparse.ArgumentParser()
+    rv.add_argument("source", help="Set path to source directory.")
+    return rv
+
+def run():
+    p = parser()
+    args = p.parse_args()
+    rv = main(args)
+    sys.exit(rv)
 
 
 if __name__ == "__main__":
