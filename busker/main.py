20436282aa11dc658db7c218055d6e74206ea85b
tundish <tundish@gigeconomy.org.uk>
2024-03-11 07:06:17 +0000

    Capture Home page for testing.

--- busker/main.py
+++ busker/main.py
@@ -1,5 +1,27 @@
+#!/usr/bin/env python3
+#   encoding: utf-8
+
+# This is part of the Busker library.
+# Copyright (C) 2024 D E Haynes
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import argparse
+import asyncio
 import sys
+
+import httpx
 
 from balladeer import discover_assets
 
@@ -7,15 +29,19 @@ import busker
 
 
 def main(args):
-    assets = discover_assets(args.source, "")
+    # assets = discover_assets(args.source, "")
     print(f"Busker {busker.__version__}", file=sys.stderr)
-    print(f"{assets}", file=sys.stdout)
+    page = httpx.get(args.url)
+    print(f"{page.text}", file=sys.stdout)
     print("Done.", file=sys.stderr)
     return 0
 
 def parser():
     rv = argparse.ArgumentParser()
-    rv.add_argument("source", help="Set path to source directory.")
+    rv.add_argument(
+        "--url", default="http://localhost:8080",
+        help="Set url path to begin session."
+    )
     return rv
 
 def run():
---
+++ busker/test/test_scraper.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+#   encoding: utf-8
+
+# This is part of the Busker library.
+# Copyright (C) 2024 D E Haynes
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import unittest
+import textwrap
+from types import SimpleNamespace
+
+
+class ScraperTests(unittest.TestCase):
+
+    fixtures = SimpleNamespace(
+        Home=textwrap.dedent("""
+        <!DOCTYPE html>
+        <html>
+        <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="/static/basics.css" />
+        <link rel="stylesheet" href="/static/layout.css" />
+        <link rel="stylesheet" href="/static/object.css" />
+        </head>
+        <body>
+        <form role="form" action="/sessions" method="POST" name="ballad-form-start">
+        <button action="submit">Begin</button>
+        </form>
+        </body>
+        </html>
+        """),
+    )
+
+    def test_find_session_begin(self):
+        self.fail(self.fixtures.Home)
