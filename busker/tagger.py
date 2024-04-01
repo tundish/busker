d3a93785fa7ed431dec34f5f8df6060bc5ad31f4
tundish <tundish@gigeconomy.org.uk>
2024-04-01 13:24:15 +0100

    Add tagger module.

---
+++ busker/tagger.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+#   encoding: utf-8
+
+# This is part of the Busker library.
+# Copyright (C) 2024 D E Haynes
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import html.parser
+
+
+class Tagger(html.parser.HTMLParser):
+
+    def __init__(self, widget, convert_charrefs=True):
+        super().__init__(convert_charrefs=convert_charrefs)
+        self.widget = widget
+        self.tags = []
+
+    def handle_starttag(self, tag, attrs):
+        print("Encountered a start tag:", tag)
+
+    def handle_endtag(self, tag):
+        print("Encountered an end tag :", tag)
+
+    def handle_data(self, data):
+        print("Encountered some data  :", data)
