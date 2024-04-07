426a44e84cb8c4a1a1b8057d1b930d188af63afe
tundish <tundish@gigeconomy.org.uk>
2024-04-07 19:57:35 +0100

    Refactoring to choose.

--- busker/visitor.py
+++ busker/visitor.py
@@ -65,7 +65,18 @@ class Visitor(SharedHistory):
         return len([i for record in self.witness.commands.values() for i in record])
 
     def choose(self, node: Node) -> Choice:
-        return None
+        choice = Choice(form=random.randrange(len(node.actions)))
+        try:
+            choice = choice._replace(input=random.randrange(len(node.actions[choice.form].inputs)))
+            choice = choice._replace(value=random.choice(node.actions[choice.form].inputs[choice.input].values))
+        except ValueError:
+            # No inputs in form
+            pass
+
+        # Back out of dead ends
+        if self.witness.commands[node.hash] and not self.witness.untested(node):
+            choice = choice._replace(value=None)
+        return choice
 
     def __call__(self, tactic, *args, **kwargs):
 
@@ -73,8 +84,6 @@ class Visitor(SharedHistory):
 
         try:
             node = tactic.run(self.scraper, **kwargs)
-            if not node.actions:
-                return node
         except urllib.error.HTTPError as e:
             value = f'{tactic.choice.value}' if tactic.choice.value is not None else "None"
             self.log(
@@ -86,19 +95,10 @@ class Visitor(SharedHistory):
 
         self.witness.update(node, tactic.choice)
 
-        choice = Choice(form=random.randrange(len(node.actions)))
-        try:
-            # TODO: Better understanding of input fields
-            choice = choice._replace(input=random.randrange(len(node.actions[choice.form].inputs)))
-            choice = choice._replace(value=random.choice(node.actions[choice.form].inputs[choice.input].values))
-        except ValueError:
-            # No inputs in form
-            pass
-
-        # Back out of dead ends
-        if self.witness.commands[node.hash] and not self.witness.untested(node):
-            choice = choice._replace(value=None)
-
+        if node.actions:
+            choice = self.choose(node)
+        else:
+            return node
         if node.url != self.url or len(self.witness.commands) == 1:
             self.tactics.append(Write(node, choice=choice))
 
