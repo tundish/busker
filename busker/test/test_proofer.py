3db4b2ab0ae841c80762ed42aaf72f54087c1b82
tundish <tundish@gigeconomy.org.uk>
2024-11-03 16:37:02 +0000

    Add test for missing role.

--- busker/test/test_proofer.py
+++ busker/test/test_proofer.py
@@ -70,3 +70,74 @@ class ProoferTests(unittest.TestCase):
         finally:
             os.close(fd)
             path.unlink()
+
+    def test_parse_role_name(self):
+        text = textwrap.dedent("""
+        [ALICE]
+        name = "Alice"
+
+        [[_]]
+        s='''
+        <BORIS>Hey, {ALICE.name}!
+        '''
+        """).strip()
+        fd, name = tempfile.mkstemp(suffix=".scene.toml", text=True)
+        path = pathlib.Path(name)
+        try:
+            path.write_text(text)
+            script = Proofer.read_script(path)
+            script = Proofer.check_scene(script)
+            self.assertTrue(script.errors, script)
+            self.assertIn(6, script.errors)
+            self.assertIn("BORIS", script.errors[6])
+        finally:
+            os.close(fd)
+            path.unlink()
+
+    def test_parse_role_directive(self):
+        text = textwrap.dedent("""
+        [ALICE]
+        name = "Alice"
+
+        [[_]]
+        s='''
+        <BORIS:proposing>Hey, {ALICE.name}!
+        '''
+        """).strip()
+        fd, name = tempfile.mkstemp(suffix=".scene.toml", text=True)
+        path = pathlib.Path(name)
+        try:
+            path.write_text(text)
+            script = Proofer.read_script(path)
+            script = Proofer.check_scene(script)
+            self.assertTrue(script.errors, script)
+            self.assertIn(6, script.errors)
+            self.assertIn("BORIS", script.errors[6])
+            self.assertNotIn("proposing", script.errors[6])
+        finally:
+            os.close(fd)
+            path.unlink()
+
+    def test_parse_role_mode(self):
+        text = textwrap.dedent("""
+        [ALICE]
+        name = "Alice"
+
+        [[_]]
+        s='''
+        <BORIS:whispers>Hey, {ALICE.name}!
+        '''
+        """).strip()
+        fd, name = tempfile.mkstemp(suffix=".scene.toml", text=True)
+        path = pathlib.Path(name)
+        try:
+            path.write_text(text)
+            script = Proofer.read_script(path)
+            script = Proofer.check_scene(script)
+            self.assertTrue(script.errors, script)
+            self.assertIn(6, script.errors)
+            self.assertIn("BORIS", script.errors[6])
+            self.assertNotIn("whispers", script.errors[6])
+        finally:
+            os.close(fd)
+            path.unlink()
