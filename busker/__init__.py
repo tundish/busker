af227afefaae7174a2c6795da992a4d01af87512
tundish <tundish@gigeconomy.org.uk>
2026-04-11 13:54:25 +0100

    Switch back to setuptools for packaging.

--- busker/__init__.py
+++ busker/__init__.py
@@ -1,8 +1,1 @@
-import datetime
-import importlib.metadata
-
-try:
-    __version__ = importlib.metadata.version("busker")
-except importlib.metadata.PackageNotFoundError:
-    __version__ = datetime.date.today().strftime("%Y.%m.%d") + "+local"
-
+__version__ = "0.23.0"
