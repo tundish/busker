fe3e4931238c1294890bb5b276919cb2333e6c8e
Tundish <tundish@gigeconomy.org.uk>
2026-08-28 17:53:19 +0100

    Minor.

--- busker/model/test/test_plotline.py
+++ busker/model/test/test_plotline.py
@@ -61,7 +61,7 @@ class PlotlineTests(unittest.TestCase):
         }
         {"mark": 2863490869328, "type": "text/x-python"}
 
-        def fn(context: dict, marker: dict, **kwargs):
+        def fn(context: dict, **kwargs):
             context["score"] += 1
 
         {"mark": 2863490869328, "type": "application/json", "path": ["a"]}
