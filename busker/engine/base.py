50cdac205c262dc87daceea0df453765f5c4130f
Tundish <tundish@gigeconomy.org.uk>
2026-08-29 11:09:46 +0100

    Quick add modules for engine prototyping.

---
+++ busker/engine/base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#   encoding: utf-8
+
+# Copyright (C) 2026 D E Haynes
+# This file is part of busker.
+
+# Busker is free software: you can redistribute it and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation, either version 3 of the License,
+# or (at your option) any later version.
+#
+# Busker is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+# the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with busker.
+# If not, see <https://www.gnu.org/licenses/>.
+
+
+class Engine:
+    pass
+
+
